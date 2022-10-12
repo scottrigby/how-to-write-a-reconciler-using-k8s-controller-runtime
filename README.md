0f1fb0f0cd730d4ae233e415b12d85104972cb9a
nikimanoledaki <niki@weave.works>
2022-10-12 17:41:37 +0200

    Add Speaker CRUD endpoint logic

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 How To Write a Reconciler Using k8s Controller-Runtime!
 
 
-# Using the provided vagrant File
+## Using the provided Vagrantfile
 
 We provide a vagrantfile that will create a VM with all the necessary tools installed.
 To use it, simply run `vagrant up` and then `vagrant ssh` to get into the VM.
@@ -10,14 +10,14 @@ You can then run `make` to build the project.
 
 Please not that building the vm will take a while, as it will download all the necessary tools.
 
-## Prerequisites
+### Prerequisites
 
 This will work with virtual box and vagrant installed on your machine.
 
 for vmware fusion, you will need to install the vagrant-vmware-fusion plugin.
 https://www.vagrantup.com/vmware/downloads
 
-## Steps
+### Steps
 
 Into a terminal run the following commands:
 
@@ -42,3 +42,73 @@ running from oracle, just allow and approve it, this problem is gone.
 
 The repository is synced to the vagrant box at `/home/vagrant/app`. You can edit
 the files in your favorite editor and run the commands in the vagrant box.
+
+## API Documentation
+
+Get the API running:
+```
+go run main.go
+```
+
+### Speakers
+
+Create a Speaker:
+```
+curl -sd '{"ID":"default/ScottRigby","name":"Scott Rigby","bio":"Scott is a rad dad","email":"scott@email.com","timestamp":"0001-01-01T00:00:00Z"}' \
+-H "Content-Type: application/json" \
+-X POST localhost:8080/api/speakers | jq
+{
+  "ID": "default/ScottRigby",
+  "Name": "Scott Rigby",
+  "Bio": "Scott is a rad dad",
+  "Email": "scott@email.com",
+  "Timestamp": "0001-01-01T00:00:00Z"
+}
+```
+
+Get all Speakers:
+```
+curl -sX GET localhost:8080/api/speakers | jq
+[
+  {
+    "ID": "default/ScottRigby",
+    "Name": "NewName",
+    "Bio": "Scott is a rad dev",
+    "Email": "scott@email.com",
+    "Timestamp": "0001-01-01T00:00:00Z"
+  }
+]
+```
+
+Get a Speakerby ID:
+```
+curl -sX GET localhost:8080/api/speakers/default-ScottRigby | jq
+[
+  {
+    "ID": "default/ScottRigby",
+    "Name": "NewName",
+    "Bio": "Scott is a rad dev",
+    "Email": "scott@email.com",
+    "Timestamp": "0001-01-01T00:00:00Z"
+  }
+]
+```
+
+Update a Speaker:
+```
+curl -sd '{"ID":"default/ScottRigby","name":"NewName","bio":"Scott is a rad dev","email":"scott@email.com","timestamp":"0001-01-01T00:00:00Z"}' \
+-H "Content-Type: application/json" \
+-X PUT localhost:8080/api/speakers/default-ScottRigby | jq
+{
+  "ID": "default/ScottRigby",
+  "Name": "NewName",
+  "Bio": "Scott is a rad dev",
+  "Email": "scott@email.com",
+  "Timestamp": "0001-01-01T00:00:00Z"
+}
+```
+
+Delete a Speaker:
+```
+curl -X DELETE localhost:8080/api/speakers/default-ScottRigby
+```
--- cfp-app/pkg/handlers/speakers.go
+++ cfp-app/pkg/handlers/speakers.go
@@ -3,25 +3,168 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/scottrigby/cfp/pkg/types"
+	"github.com/scottrigby/cfp/pkg/utils"
 )
 
+const speakerDataPath = "data/speakers/"
+
+// CreateSpeaker creates a new file with json data about a given Speaker.
 func CreateSpeaker(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	var speaker types.Speaker
 	json.NewDecoder(r.Body).Decode(&speaker)
-	file, _ := json.MarshalIndent(speaker, "", " ")
-	_ = ioutil.WriteFile(fmt.Sprintf("data/speakers/%v.json", speaker.ID), file, 0644)
+
+	if err := validateSpeaker(&speaker); err != nil {
+		utils.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if speakerExists(speaker.ID) {
+		utils.Error(w, fmt.Sprintf("speaker with ID '%s' already exists", speaker.ID), http.StatusBadRequest)
+		return
+	}
+
+	writeSpeaker(w, r, &speaker)
+}
+
+// GetSpeakerById returns the data for a Speaker given the Speaker's ID.
+func GetSpeakerById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id := mux.Vars(r)["id"]
+	if id == "" {
+		utils.Error(w, "speaker ID must be specified", http.StatusBadRequest)
+		return
+	}
+
+	b, err := os.ReadFile(fmt.Sprintf("%s%s.json", speakerDataPath, utils.MakeFileName(id)))
+
+	switch {
+	case len(b) == 0:
+		utils.Error(w, fmt.Sprintf("failed to find Speaker with ID '%s'", id), http.StatusNotFound)
+		return
+	case err != nil:
+		utils.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var speaker types.Speaker
+	if err := json.Unmarshal(b, &speaker); err != nil {
+		utils.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(speaker)
 }
 
-func GetSpeakers(w http.ResponseWriter, r *http.Request) {}
+// GetSpeakerById returns a list with the data of all the files of Speakers
+// contained in "data/speakers/"".
+func GetSpeakers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-func GetSpeakerById(w http.ResponseWriter, r *http.Request) {}
+	files, err := os.ReadDir(speakerDataPath)
+	if err != nil {
+		utils.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-func UpdateSpeaker(w http.ResponseWriter, r *http.Request) {}
+	var speakerList []types.Speaker
+	for _, file := range files {
+		b, err := os.ReadFile(fmt.Sprintf("%s%s", speakerDataPath, file.Name()))
 
-func DeleteSpeaker(w http.ResponseWriter, r *http.Request) {}
+		switch {
+		case len(b) == 0:
+			continue
+		case err != nil:
+			utils.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var speaker types.Speaker
+		if err := json.Unmarshal(b, &speaker); err != nil {
+			utils.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		speakerList = append(speakerList, speaker)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(speakerList)
+}
+
+// UpdateSpeaker checks that a file for a Speaker exists given their ID
+// then replaces the json data for that Speaker by overwriting it.
+func UpdateSpeaker(w http.ResponseWriter, r *http.Request) {
+	var speaker types.Speaker
+	json.NewDecoder(r.Body).Decode(&speaker)
+	id := mux.Vars(r)["id"]
+
+	if utils.MakeFileName(speaker.ID) != id {
+		utils.Error(w, fmt.Sprintf("ID '%s' used as query param does not match ID in request body '%s'", id, speaker.ID), http.StatusBadRequest)
+		return
+	}
+
+	if speaker.ID == "" {
+		speaker.ID = id
+	}
+
+	if err := validateSpeaker(&speaker); err != nil {
+		utils.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if !speakerExists(speaker.ID) {
+		utils.Error(w, fmt.Sprintf("speaker with ID '%s' does not exist", id), http.StatusNotFound)
+		return
+	}
+
+	writeSpeaker(w, r, &speaker)
+}
+
+// DeleteSpeaker deletes the file with data for a Speaker given their ID.
+func DeleteSpeaker(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	if !speakerExists(id) {
+		utils.Error(w, fmt.Sprintf("speaker with ID '%s' does not exist", id), http.StatusNotFound)
+		return
+	}
+
+	if err := os.Remove(fmt.Sprintf("%s%s.json", speakerDataPath, utils.MakeFileName(id))); err != nil {
+		utils.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// writeSpeaker writes or overwrites a Speaker.
+func writeSpeaker(w http.ResponseWriter, r *http.Request, speaker *types.Speaker) {
+	content, _ := json.MarshalIndent(speaker, "", " ")
+
+	speaker.Timestamp = time.Now()
+
+	_ = os.WriteFile(fmt.Sprintf("%s%v.json", speakerDataPath, utils.MakeFileName(speaker.ID)), content, 0644)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(speaker)
+}
+
+func speakerExists(id string) bool {
+	b, _ := os.ReadFile(fmt.Sprintf("%s%s.json", speakerDataPath, utils.MakeFileName(id)))
+	return len(b) > 0
+}
+
+func validateSpeaker(speaker *types.Speaker) error {
+	if speaker.ID == "" || speaker.Name == "" || speaker.Email == "" {
+		return fmt.Errorf("speaker ID, Name and Email must be provided")
+	}
+
+	return nil
+}
