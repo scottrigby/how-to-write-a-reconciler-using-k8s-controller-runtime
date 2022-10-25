5a08beec90bfb72fbf2ce7136416dcb9325bfd3f
Soule BA <bah.soule@gmail.com>
2022-10-25 16:22:15 -0400

    step4: create proposal reconciliation

--- cfp/controllers/proposal_controller.go
+++ cfp/controllers/proposal_controller.go
@@ -18,13 +18,37 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"time"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/fluxcd/pkg/apis/meta"
+	"github.com/fluxcd/pkg/runtime/conditions"
+	"github.com/fluxcd/pkg/runtime/patch"
 	talksv1 "github.com/scottrigby/how-to-write-a-reconciler-using-k8s-controller-runtime/cfp/api/v1"
+	"github.com/scottrigby/how-to-write-a-reconciler-using-k8s-controller-runtime/cfp/internal/cfp"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var proposalOwnedConditions = []string{
+	meta.ReadyCondition,
+	meta.ReconcilingCondition,
+	meta.StalledCondition,
+	talksv1.CreateFailedCondition,
+	talksv1.UpdateFailedCondition,
+	talksv1.FetchFailedCondition,
+}
 
 // ProposalReconciler reconciles a Proposal object
 type ProposalReconciler struct {
@@ -55,5 +79,209 @@ func (r *ProposalReconciler) SetupWithManager(mgr ctrl.Manager) error {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
 func (r *ProposalReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, retErr error) {
+	log := log.FromContext(ctx)
+
+	// Fetch the proposal
+	// Automatically requeue if an error is returned
+	// otherwise requeue based on the result.requeue and result.requeueAfter
+	obj := &talksv1.Proposal{}
+	if err := r.Get(ctx, req.NamespacedName, obj); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	log.Info("reconciling proposal", "speaker", obj.Name)
+
+	// Initialize the patch helper with the current version of the object.
+	// Helper is a utility for ensuring the proper patching of objects.
+	patchHelper, err := patch.NewHelper(obj, r.Client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Always attempt to Patch the Proposal object and status after each reconciliation.
+	defer func() {
+		// Patch the object, ignoring conflicts on the conditions owned by this controller
+		patchOpts := []patch.Option{
+			patch.WithOwnedConditions{
+				Conditions: proposalOwnedConditions,
+			},
+		}
+
+		patchOpts = append(patchOpts, patch.WithFieldOwner(r.ControllerName))
+
+		// Set status observed generation field if the object is stalled, or ready.
+		// See https://alenkacz.medium.com/kubernetes-operator-best-practices-implementing-observedgeneration-250728868792
+		if conditions.IsStalled(obj) || conditions.IsReady(obj) {
+			patchOpts = append(patchOpts, patch.WithStatusObservedGeneration{})
+		}
+
+		// Finally, patch the resource
+		if err := patchHelper.Patch(ctx, obj, patchOpts...); err != nil {
+			if !obj.GetDeletionTimestamp().IsZero() {
+				err = kerrors.FilterOut(err, func(e error) bool { return apierrors.IsNotFound(e) })
+			}
+
+			retErr = kerrors.NewAggregate([]error{retErr, err})
+		}
+
+	}()
+
+	// Set a finalizer on the obj object if not set
+	// Finalizers are keys on resources that signal pre-delete operations. They control the garbage collection on resources,
+	// and are designed to alert controllers what cleanup operations to perform prior to removing a resource.
+	// https://kubernetes.io/blog/2021/05/14/using-finalizers-to-control-deletion/
+	if !controllerutil.ContainsFinalizer(obj, talksv1.Finalizer) {
+		controllerutil.AddFinalizer(obj, talksv1.Finalizer)
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	//create a new propsal client
+	cfpClient, err := cfp.NewClient(r.CfpAPI, r.HTTPClient)
+	if err != nil {
+		conditions.MarkStalled(obj, talksv1.CreateFailedCondition, "Failed to create CFP client")
+		return ctrl.Result{}, err
+	}
+
+	// Perform reconciliation logic
+	result, retErr = r.reconcile(ctx, obj, cfpClient)
+
 	return
+}
+
+func (r *ProposalReconciler) reconcile(ctx context.Context, obj *talksv1.Proposal, client *cfp.Client) (result ctrl.Result, retErr error) {
+	// defer func attempt to set the Ready condition and unset all needed conditions based on the reconciliation
+	defer func() {
+		if !result.Requeue && retErr == nil {
+			conditions.Delete(obj, meta.ReconcilingCondition)
+			conditions.Delete(obj, talksv1.CreateFailedCondition)
+			conditions.Delete(obj, talksv1.FetchFailedCondition)
+			conditions.MarkTrue(obj, meta.ReadyCondition, meta.SucceededReason, "reconciled '%s' successfully", obj.Name)
+		}
+
+		if retErr != nil {
+			var apiErr *cfp.Error
+			if ok := errors.As(retErr, &apiErr); ok {
+				switch apiErr.Reason {
+				case cfp.ErrCreateProposal:
+					conditions.MarkTrue(obj, talksv1.CreateFailedCondition, apiErr.Reason.Reason, apiErr.Error())
+					conditions.MarkFalse(obj, meta.ReadyCondition, apiErr.Reason.Reason, apiErr.Error())
+				case cfp.ErrCreateRequest, cfp.ErrMakeRequest:
+					conditions.MarkFalse(obj, meta.ReadyCondition, apiErr.Reason.Reason, apiErr.Error())
+				default:
+					conditions.MarkFalse(obj, meta.ReadyCondition, meta.FailedReason, apiErr.Error())
+				}
+			}
+		}
+	}()
+
+	// Set the initial status of the object to be reconciling
+	if obj.Generation != obj.Status.ObservedGeneration {
+		conditions.MarkReconciling(obj, meta.ProgressingReason, fmt.Sprintf("Reconciling a new generation of the object %d", obj.Generation))
+	}
+
+	// Get the speaker reference
+	speaker := &talksv1.Speaker{}
+	namespacedName := types.NamespacedName{Namespace: obj.Namespace, Name: obj.Spec.SpeakerRef.Name}
+	if obj.Spec.SpeakerRef.Namespace != "" {
+		namespacedName.Namespace = obj.Spec.SpeakerRef.Namespace
+	}
+
+	if err := r.Get(ctx, namespacedName, speaker); err != nil {
+		err = fmt.Errorf("unable to get speaker %s: %w", namespacedName.String(), err)
+		conditions.MarkTrue(obj, talksv1.FetchFailedCondition, talksv1.FetchFailedCondition, err.Error())
+		conditions.MarkFalse(obj, meta.ReadyCondition, meta.FailedReason, err.Error())
+		return ctrl.Result{}, err
+	}
+
+	if speaker.Status.ID == "" {
+		err := fmt.Errorf("unable to get speaker %s", namespacedName.String())
+		conditions.MarkTrue(obj, talksv1.FetchFailedCondition, talksv1.FetchFailedCondition, err.Error())
+		conditions.MarkFalse(obj, meta.ReadyCondition, meta.FailedReason, err.Error())
+		return ctrl.Result{}, err
+	}
+
+	speakerID := speaker.Status.ID
+
+	// If we have a Submission on the ProposalStatus sub resource
+	// Check if an update is needed
+	// If the proposal is marked final, and the submission status is not final, create an entry in cfp.
+	var (
+		response *ProposalObject
+		err      error
+	)
+	if obj.Status.Submission != "" {
+		return ctrl.Result{}, nil
+	}
+
+	// Create the Proposal
+	response, err = r.createProposal(ctx, obj, speakerID, client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	obj.Status.Submission = response.Submission.Status
+	obj.Status.LastUpdate = metav1.Time{Time: response.Submission.LastUpdate}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *ProposalReconciler) createProposal(ctx context.Context, obj *talksv1.Proposal, speakerID string, client *cfp.Client) (*ProposalObject, error) {
+	submissionStatus := talksv1.ProposalStateDraft
+	if obj.Spec.Final {
+		submissionStatus = talksv1.ProposalStateFinal
+	}
+
+	proposal, err := createProposalPayload(obj, speakerID, submissionStatus)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the proposal
+	resp, err := client.Create(ctx, cfp.ProposalPath, proposal)
+	if err != nil {
+		return nil, err
+	}
+
+	p := &ProposalObject{}
+	err = json.Unmarshal(resp, p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+func createProposalPayload(obj *talksv1.Proposal, speakerID, submission string) ([]byte, error) {
+	body := ProposalObject{
+		ID:         fmt.Sprintf("%s-%s", obj.Namespace, obj.Name),
+		Title:      obj.Spec.Title,
+		Abstract:   obj.Spec.Abstract,
+		Type:       obj.Spec.Type,
+		SpeakerID:  speakerID,
+		Final:      obj.Spec.Final,
+		Submission: Submission{Status: submission},
+	}
+
+	payload, err := json.Marshal(body)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling body: %w", err)
+	}
+
+	return payload, nil
+}
+
+// ProposalObject is the object that is sent to/received from the CFP API
+type ProposalObject struct {
+	ID         string     `json:"id"`
+	Title      string     `json:"title"`
+	Abstract   string     `json:"abstract"`
+	Type       string     `json:"type"`
+	SpeakerID  string     `json:"speakerID"`
+	Final      bool       `json:"final"`
+	Submission Submission `json:"submission"`
+}
+
+type Submission struct {
+	Status     string    `json:"status"`
+	LastUpdate time.Time `json:"lastUpdate"`
 }
--- cfp/controllers/proposal_controller_test.go
+++ cfp/controllers/proposal_controller_test.go
@@ -1,7 +1,192 @@
 package controllers
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/fluxcd/pkg/apis/meta"
+	"github.com/fluxcd/pkg/runtime/conditions"
+	"github.com/fluxcd/pkg/runtime/patch"
+	. "github.com/onsi/gomega"
+	talksv1 "github.com/scottrigby/how-to-write-a-reconciler-using-k8s-controller-runtime/cfp/api/v1"
 )
 
-func Test_Proposal_Reconcile(t *testing.T) {}
+func Test_Proposal_Reconcile(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	testCases := []struct {
+		name             string
+		title            string
+		abstract         string
+		proposalType     string
+		final            bool
+		assertConditions []metav1.Condition
+		beforeFunc       func(obj *talksv1.Proposal)
+		assertFunc       func(obj *talksv1.Proposal, speaker *talksv1.Speaker, assertConditions []metav1.Condition)
+	}{
+		{
+			name:         "test create proposal reconciliation without existing speaker",
+			title:        "this is a test proposal",
+			abstract:     "this is a test abstract",
+			proposalType: "lightning",
+			final:        true,
+			assertConditions: []metav1.Condition{
+				*conditions.TrueCondition(meta.ReconcilingCondition, meta.ProgressingReason, "Reconciling a new generation of the object <generation>"),
+				*conditions.FalseCondition(meta.ReadyCondition, meta.FailedReason, "unable to get speaker <namespacedName>: <group> \"<name>\" not found"),
+				*conditions.TrueCondition(talksv1.FetchFailedCondition, talksv1.FetchFailedReason, "unable to get speaker <namespacedName>: <group> \"<name>\" not found"),
+			},
+			beforeFunc: func(obj *talksv1.Proposal) {
+				key := client.ObjectKey{Name: obj.Name, Namespace: obj.Namespace}
+				// Wait for Speaker to be Ready
+				g.Eventually(func() bool {
+					if err := testEnv.Get(ctx, key, obj); err != nil {
+						return false
+					}
+					if !conditions.IsReady(obj) {
+						return false
+					}
+					readyCondition := conditions.Get(obj, meta.ReadyCondition)
+					return obj.Generation == readyCondition.ObservedGeneration &&
+						obj.Generation == obj.Status.ObservedGeneration
+				}, timeout).Should(BeTrue())
+
+				patch, err := patch.NewHelper(obj, testEnv)
+				g.Expect(err).ToNot(HaveOccurred())
+				// Set wrong speaker name
+				obj.Spec.SpeakerRef = &talksv1.SpeakerRef{
+					Name: "unkown",
+				}
+				g.Expect(patch.Patch(ctx, obj)).To(Succeed())
+			},
+			assertFunc: func(obj *talksv1.Proposal, _ *talksv1.Speaker, assertConditions []metav1.Condition) {
+				key := client.ObjectKey{Name: obj.Name, Namespace: obj.Namespace}
+				// Wait for finalizer to be set
+				g.Eventually(func() bool {
+					if err := testEnv.Get(ctx, key, obj); err != nil {
+						return false
+					}
+					return len(obj.Finalizers) > 0
+				}, timeout).Should(BeTrue())
+
+				// Wait for proposal to not be Ready
+				g.Eventually(func() bool {
+					if err := testEnv.Get(ctx, key, obj); err != nil {
+						return false
+					}
+					if !conditions.IsFalse(obj, meta.ReadyCondition) {
+						return false
+					}
+					return true
+				}, timeout).Should(BeTrue())
+
+				for k := range assertConditions {
+					assertConditions[k].Message = strings.ReplaceAll(assertConditions[k].Message, "<name>", obj.Spec.SpeakerRef.Name)
+					assertConditions[k].Message = strings.ReplaceAll(assertConditions[k].Message, "<generation>", fmt.Sprint(obj.Generation))
+					assertConditions[k].Message = strings.ReplaceAll(assertConditions[k].Message, "<namespacedName>", fmt.Sprintf("%s/%s", obj.Namespace, obj.Spec.SpeakerRef.Name))
+					assertConditions[k].Message = strings.ReplaceAll(assertConditions[k].Message, "<group>", fmt.Sprintf("Speaker.%s", talksv1.GroupVersion.Group))
+				}
+				g.Expect(obj.Status.Conditions).To(conditions.MatchConditions(assertConditions))
+
+			},
+		},
+		{
+			name:         "test create proposal reconciliation with existing speaker",
+			title:        "this is a test proposal",
+			abstract:     "this is a test abstract",
+			proposalType: "lightning",
+			final:        true,
+			assertConditions: []metav1.Condition{
+				*conditions.TrueCondition(meta.ReadyCondition, meta.SucceededReason, "reconciled '<name>' successfully"),
+			},
+			assertFunc: func(obj *talksv1.Proposal, _ *talksv1.Speaker, assertConditions []metav1.Condition) {
+				key := client.ObjectKey{Name: obj.Name, Namespace: obj.Namespace}
+				// Wait for Proposal to be Ready
+				g.Eventually(func() bool {
+					if err := testEnv.Get(ctx, key, obj); err != nil {
+						return false
+					}
+					if !conditions.IsReady(obj) {
+						return false
+					}
+					readyCondition := conditions.Get(obj, meta.ReadyCondition)
+					return obj.Generation == readyCondition.ObservedGeneration &&
+						obj.Generation == obj.Status.ObservedGeneration
+				}, timeout).Should(BeTrue())
+
+				for k := range assertConditions {
+					assertConditions[k].Message = strings.ReplaceAll(assertConditions[k].Message, "<name>", obj.Name)
+				}
+				g.Expect(obj.Status.Conditions).To(conditions.MatchConditions(assertConditions))
+
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ns, err := testEnv.CreateNamespace(ctx, "proposal-ns")
+			g.Expect(err).NotTo(HaveOccurred())
+			defer func() {
+				g.Expect(testEnv.Delete(ctx, ns)).To(Succeed())
+			}()
+
+			speaker := &talksv1.Speaker{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "speaker",
+					Namespace: ns.Name,
+				},
+				Spec: talksv1.SpeakerSpec{
+					Name:  "test",
+					Bio:   "test speaker",
+					Email: "test.speaker@gmail.com",
+				},
+			}
+
+			err = testEnv.CreateAndWait(ctx, speaker)
+			g.Expect(err).NotTo(HaveOccurred())
+
+			key := client.ObjectKey{Name: speaker.Name, Namespace: speaker.Namespace}
+			// Wait for Speaker to be Ready
+			g.Eventually(func() bool {
+				if err := testEnv.Get(ctx, key, speaker); err != nil {
+					return false
+				}
+				if !conditions.IsReady(speaker) {
+					return false
+				}
+				readyCondition := conditions.Get(speaker, meta.ReadyCondition)
+				return speaker.Generation == readyCondition.ObservedGeneration &&
+					speaker.Generation == speaker.Status.ObservedGeneration
+			}, timeout).Should(BeTrue())
+
+			obj := &talksv1.Proposal{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "proposal",
+					Namespace: ns.Name,
+				},
+				Spec: talksv1.ProposalSpec{
+					Title:    tc.title,
+					Abstract: tc.abstract,
+					Type:     tc.proposalType,
+					Final:    tc.final,
+					SpeakerRef: &talksv1.SpeakerRef{
+						Name: speaker.Name,
+					},
+				},
+			}
+
+			err = testEnv.CreateAndWait(ctx, obj)
+			g.Expect(err).NotTo(HaveOccurred())
+
+			if tc.beforeFunc != nil {
+				tc.beforeFunc(obj)
+			}
+
+			tc.assertFunc(obj, speaker, tc.assertConditions)
+		})
+	}
+}
